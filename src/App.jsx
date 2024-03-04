45d37c27d7ccb6bfc43206310a56ad74f5f2331e
Brendan McBryan <brendan.mcbryan@gmail.com>
2024-03-04 14:46:09 -0800

    Routing and subrouting

--- src/App.jsx
+++ src/App.jsx
@@ -11,11 +11,16 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Homepage />} />
+        <Route index element={<Homepage />} />
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
         <Route path="login" element={<Login />} />
-        <Route path="app" element={<AppLayout />} />
+        <Route path="app" element={<AppLayout />}>
+          <Route index element={<p>list of anything</p>} />
+          <Route path="cities" element={<p>city list here</p>} />
+          <Route path="countries" element={<p>countries list here</p>} />
+          <Route path="form" element={<p>form list here</p>} />
+        </Route>
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
