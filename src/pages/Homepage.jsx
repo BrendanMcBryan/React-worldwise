a70000746b4569598ccc10f2acf4a8f2310752be
Brendan McBryan <brendan.mcbryan@gmail.com>
2024-03-04 13:48:02 -0800

    Initial Commit for Vite Project

---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,17 @@
+import { Link } from 'react-router-dom';
+import PageNav from '../components/PageNav';
+import AppNav from '../components/AppNav'
+
+function Homepage() {
+  return (
+    <div>
+      <PageNav />
+      <AppNav />
+      <h1 className='test'>worldwise</h1>
+
+      <Link to="/app">App</Link>
+    </div>
+  );
+}
+
+export default Homepage;
---
+++ src/pages/PageNotFound.jsx
@@ -0,0 +1,13 @@
+import PageNav from '../components/PageNav'
+
+function PageNotFound() {
+  return (
+    <div>
+
+    <PageNav />
+      <h1>No Page Found</h1>
+    </div>
+  )
+}
+
+export default PageNotFound
