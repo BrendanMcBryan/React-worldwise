62affc62492ad557167efaf0ffb08860d4742dd9
Brendan McBryan <brendan.mcbryan@gmail.com>
2024-03-05 11:14:58 -0800

    useNavigate

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,11 +1,31 @@
-import styles from './Map.module.css'
+import { useNavigate, useSearchParams } from 'react-router-dom';
+import styles from './Map.module.css';
 
 function Map() {
+  const navigate = useNavigate();
+
+  const [searchParams, setSearchParams] = useSearchParams();
+  const lat = searchParams.get('lat');
+  const lng = searchParams.get('lng');
+
   return (
-    <div className={styles.mapContainer}>
-      map
+    <div
+      className={styles.mapContainer}
+      onClick={() => {
+        navigate('form');
+      }}
+    >
+      <h1>latittude: {lat}</h1>
+      <h1>longtidue: {lng}</h1>
+      <button
+        onClick={() => {
+          setSearchParams({ lat: 23, lng: 50 });
+        }}
+      >
+        change position
+      </button>{' '}
     </div>
-  )
+  );
 }
 
-export default Map
+export default Map;
